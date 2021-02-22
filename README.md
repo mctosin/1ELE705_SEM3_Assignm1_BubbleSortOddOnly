30792f4f1a93278573006ecf4aa48f97c1e3f895
mctosin <39802158+mctosin@users.noreply.github.com>
2021-02-21 23:13:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1ELE705_SEM3_Assignm1_BubbleSort
+Template para o algoritmo Bubble Sort
+
+Neste assignment você deve criar um programa para ordenar em ordem crescente o vetor de float a[] usando o método de ordenação Bubble Sort descrito na vídeo aula correspondente.
+Os detalhes para escrever seu código encontram-se nos comentários do arquivo main.c deste template.
---
+++ main.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv) {
+
+	float a[1000];
+	int i, Q_elem_a;
+
+	for (i = 1; i < argc; i++) {
+		a[i - 1] = atof(argv[i]);
+		fprintf(stderr, "%.2f ", a[i - 1]);
+	}
+	fprintf(stderr, "\n");
+	Q_elem_a = argc - 1;
+
+	// Não modifique o código acima
+
+	// Codifique o algoritmo Bubble Sort para  
+	// ordenar em ordem crescente o vetor de
+	// float a[], o qual possui Q_elem_a elementos.
+
+
+	
+	// A saída deve ser para a stdout e deve ser na forma de 
+	// uma string contendo os elementos do vetor em ordem
+	// crescente, serados por um espaço entre eles.
+	// O último caractere da string deve ser o
+	// pula linha '\n'. 
+	// Exemplo: "<elem1>< ><elem2>< > ... < ><elemN><\n>"
+	// <  > servem para representar os componentes da string
+	// e não devem ser impressos.
+	// Os valores devem ser arredondados com 2 casas decimais.
+	// Crie a saída para o programa de testes automático aqui
+
+	
+
+	// Não modifique o código abaixo
+
+	return 0;
+}
+
